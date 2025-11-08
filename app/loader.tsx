12364076c3ae8f6845ea95affab1f8a59bdd9e6f
raphaelcaleb4 <calpacino.cr@gmail.com>
2025-11-08 19:55:55 +0100

    Test Commit #1

---
+++ app/loader.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Loader = () => {
+    return (
+        <div>__Spinner__</div>
+    )
+}
+export default Loader
---
+++ component/hello.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+const Hello = () => {
+    console.log("CLient component");
+    return (
+        <div>Hello</div>
+    )
+}
+export default Hello
